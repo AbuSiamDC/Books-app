a29655e4b36aedad573835e85735cba757f65fcc
Abu Siam <asiam173007@bscse.uiu.ac.bd>
2023-01-25 16:02:17 +0600

    try to upload image but not complete it

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -12,6 +12,8 @@ const Add = () => {
     cover: ""
   });
 
+  const [file,setFile] = useState(null);
+
   const navigate = useNavigate()
 
   
@@ -20,9 +22,27 @@ const Add = () => {
   }
   console.log(input);
 
+  const upload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      // console.log(file);
+      setTimeout(1000)
+      const res = await axios.post("http://localhost:8800/upload", formData);
+      // console.log(formData);
+      // console.log(res.data);
+      return res.data
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleClick = async (event) => {
     event.preventDefault()
+    const imgurl = await upload()
+    console.log("image url");
+    console.log(imgurl);
+    setInput((prev) => ({...prev, cover:imgurl}))
     try {
       await axios.post("http://localhost:8800/books", input)
       navigate("/")
@@ -38,10 +58,90 @@ const Add = () => {
       <input type="text" placeholder="title" onChange={handleChange} name="title"/>
       <input type="text" placeholder="desc" onChange={handleChange} name="desc"/>
       <input type="number" placeholder="price" onChange={handleChange} name="price"/>
-      <input type="text" placeholder="cover" onChange={handleChange} name="cover"/>
+      {/* <input type="text" placeholder="cover" onChange={handleChange} name="cover"/> */}
+      {/* <input type="file" placeholder="cover" onChange={handleChange} name="cover"/> */}
+      <input type="file" placeholder="cover" onChange={event => setFile(event.target.files[0])} name="cover"/>
       <button className="formButton" onClick={handleClick}>ADD</button>
     </div>
   );
 };
 
 export default Add;
+
+
+// import axios from "axios";
+// import React from "react";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+
+// const Add = () => {
+
+//   const [title,setTitle] = useState('');
+//   const [desc,setDesc] = useState('');
+//   const [price,setPrice] = useState(null);
+//   const [file,setFile] = useState(null);
+
+//   const navigate = useNavigate()
+
+  
+//   // const handleChange = (event) => {
+//   //   setInput((prev) => ({...prev, [event.target.name]: event.target.value}))
+//   // }
+
+//   const handleTitle = (event) => {
+//     setTitle(event.target.value)
+//   }
+//   const handleDesc = (event) => {
+//     setTitle(event.target.value)
+//   }
+//   const handlePrice = (event) => {
+//     setTitle(event.target.value)
+//   }
+
+
+
+//   const upload = async () => {
+//     try {
+//       const formData = new FormData();
+//       formData.append("file", file);
+//       // console.log(file);
+//       const res = await axios.post("http://localhost:8800/upload", formData);
+//       // console.log(formData);
+//       // console.log(res.data);
+//       return res.data
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   const handleClick = async (event) => {
+//     event.preventDefault()
+//     const imgurl = await upload()
+//     console.log("image url");
+//     console.log(imgurl);
+//     setInput((prev) => ({...prev, cover:imgurl}))
+//     try {
+//       await axios.post("http://localhost:8800/books", input)
+//       navigate("/")
+
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+
+//   return (
+//     <div className="form">
+//       <h1>Add new book</h1>
+//       <input type="text" placeholder="title" onChange={handleTitle} name="title"/>
+//       <input type="text" placeholder="desc" onChange={handleDesc} name="desc"/>
+//       <input type="number" placeholder="price" onChange={handlePrice} name="price"/>
+//       {/* <input type="text" placeholder="cover" onChange={handleChange} name="cover"/> */}
+//       {/* <input type="file" placeholder="cover" onChange={handleChange} name="cover"/> */}
+//       <input type="file" placeholder="cover" onChange={event => setFile(event.target.files[0])} name="cover"/>
+//       <button className="formButton" onClick={handleClick}>ADD</button>
+//     </div>
+//   );
+// };
+
+// export default Add;
